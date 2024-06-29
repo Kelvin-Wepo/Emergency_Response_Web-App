cb501c41f3f6834498d244011b1501c53f03aef4
kelvin wepo <kelvinwepo7710@gmail.com>
2024-06-29 10:56:03 +0300

    this commit modifies admin.py

--- ambulance/admin.py
+++ ambulance/admin.py
@@ -1,3 +1,9 @@
+# Register your models here.
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Feedback)
+admin.site.register(Ambulance)
+admin.site.register(Booking)
+admin.site.register(Profile)
